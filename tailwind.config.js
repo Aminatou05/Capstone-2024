5fee6b8f49c458256121377263ec530c217b7839
jallowaminatou@yahoo.com <jalllowaminatou@yahoo.com>
2024-04-27 11:30:41 -0400

    project set up and install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", //Any tailwind classes i add is going to  be detected with these extentions
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
