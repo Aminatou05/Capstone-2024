ede96e39268ae6d20f1c5126b077f620558e611f
jallowaminatou@yahoo.com <jalllowaminatou@yahoo.com>
2024-04-28 12:28:03 -0400

    create a header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+
+     <header className='bg-slate-400 shadow-md'>
+    <div className='flex justify-between items-center max-w-6xl mx-auto p-4'>
+        <Link to='/'>
+    <h1 className='font-bold text-sm sm:text-xl flex-wrap'>
+        <span className='text-slate-500'>LENCO</span>
+        <span className='text-slate-800'>REALTY</span>
+    </h1>
+    </Link>
+    <form  className='bg-slate-100 p-3 rounded-lg flex items-center'>
+<input type="text" placeholder='Search...' 
+//  responsiveness for diferrent devices
+className='bg-transparent focus: outline-none w-24 sm:w-64' /> 
+{/* using react icons on my search form */}
+<FaSearch className='text-slate-500'/>
+    </form>
+    <ul className='flex gap-4'>
+    <Link to='/'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              Home
+            </li>
+            </Link>
+            <Link to='/about'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              About
+            </li>
+          </Link>
+          <Link to='/sign-in'>
+        <li  className=' text-slate-700 hover:underline'>Sign in</li>
+        </Link>
+    </ul>
+    </div>
+   </header>
+  
+  )
+}
