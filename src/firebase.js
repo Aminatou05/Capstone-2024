d62dc36141ccf958780246b8473a32327c2d0ca7
jallowaminatou@yahoo.com <jalllowaminatou@yahoo.com>
2024-05-02 07:49:50 -0400

    create and add OAuth component

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAs2g_heY5KXF468rdg_LwuGDhqlw8wrXQ",
+  authDomain: "capstone2-96276.firebaseapp.com",
+  projectId: "capstone2-96276",
+  storageBucket: "capstone2-96276.appspot.com",
+  messagingSenderId: "98838612758",
+  appId: "1:98838612758:web:eddddaa89dd8b0f2c45e37"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -6,6 +6,7 @@ import {
   signInSuccess,
   signInFailure,
 } from '../redux/user/userSlice';
+import OAuth from "../components/OAuth";
 
 /**
  * Sign-in component with form for users to enter email and password.
@@ -75,6 +76,7 @@ export default function SignIn() {
          className="bg-slate-800 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
         {loading ? 'Loading...' : 'Sign In'}
         </button>
+        <OAuth/>
       </form>
       <div className="flex gap-2 mt-5">
         <p> Dont have an account?</p>
